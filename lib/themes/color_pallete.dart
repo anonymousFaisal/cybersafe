eddbfad4fe76bbb6b791c3117e4632555c91e6af
Nahid Hasan <109328601+anonymousFaisal@users.noreply.github.com>
2024-02-09 11:47:06 +0600

    added logo and color pallete

---
+++ lib/themes/color_pallete.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color onyx = Color(0xFF404042);
+  static const Color ultraViolet = Color(0xFF645B89);
+  static const Color wisteria = Color(0xFFA4A1D7);
+  static const Color black = Color(0xFF080809);
+  static const Color davysGray = Color(0xFF605E65);
+  static const Color ultraViolet2 = Color(0xFF6B638D);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color delftBlue = Color(0xFF40356F);
+  static const Color coolGray = Color(0xFF888499);
+}
